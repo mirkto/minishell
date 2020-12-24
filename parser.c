23b7b76b89dea3522d9c6b99be79ab87bed127f5
Armand Annara <arannara@il-k3.msk.21-school.ru>
2020-12-24 21:22:10 +0300

    adding parser funk

--- parser.c
+++ parser.c
@@ -3,38 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ngonzo <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: arannara <arannara@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/02 16:55:10 by ngonzo            #+#    #+#             */
-/*   Updated: 2020/12/02 16:55:14 by ngonzo           ###   ########.fr       */
+/*   Updated: 2020/12/24 21:19:50 by arannara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		check_comma(char *tmp, t_param *all)
-{
-	char	num_c;
-	int		c1;
-	int		c2;
+// int		check_comma(char *tmp, t_param *all)
+// {
+// 	char	num_c;
+// 	int		c1;
+// 	int		c2;
 
-	num_c = 0;
-	all->i = 0;
-	c1 = 0;
-	c2 = 0;
-	all->flag = 0;
-	while (all->i < all->buf_len)
+// 	num_c = 0;
+// 	all->i = 0;
+// 	c1 = 0;
+// 	c2 = 0;
+// 	all->flag = 0;
+// 	while (all->i < all->buf_len)
+// 	{
+// 		if (tmp[all->i] == '\'')
+// 			c1++;
+// 		if (tmp[all->i] == '"')
+// 			c2++;
+// 		all->i++;
+// 	}
+// 	if ((c1 % 2) != 0 || (c2 % 2) != 0)
+// 		all->flag = -1;
+// 	return (0);
+// }
+
+int lexer(char *tmp)
+{
+	int i; // obrabotka ";;" "cmd ;" "|" ";"
+
+	if ((tmp[0]) == '|' || tmp[0] == ';')
+		return (put_error("syntax ERROR", 0));
+	i = 0;
+	while (tmp[i])
 	{
-		if (tmp[all->i] == '\'')
-			c1++;
-		if (tmp[all->i] == '"')
-			c2++;
-		all->i++;
+		if (tmp[i] == ';' && (tmp[i + 1]) == '\0')
+			return (put_error("syntax ERROR", 0));
+		if (tmp[i] == ';' && (tmp[i + 1]) == ';')
+			return (put_error("syntax error near unexpected token `;;'", 0));
+		i++;
 	}
-	if ((c1 % 2) != 0 || (c2 % 2) != 0)
-		all->flag = -1;
+	i = 0;
+	while (tmp[i])   // obrabotka ";      ;"
+	{
+		if (tmp[i] == ';')
+		{
+			i++;
+			while (tmp[i] == ' ')
+				i++;
+			if (tmp[i] == ';')
+				return (put_error("syntax error near unexpected token `;;'", 0));
+		}
+		i++;
+	}
 	return (0);
 }
+
 
 int		parser(t_param *all, char **buf)
 {
@@ -42,7 +74,8 @@ int		parser(t_param *all, char **buf)
 
 	tmp = ft_strtrim(*buf, "\v\f\r \n\t");
 	all->buf_len = ft_strlen(tmp);
-	check_comma(tmp, all);
+	// check_comma(tmp, all);
+	lexer(tmp);
 	if (all->buf_len > 0 && all->flag != -1)
 	{
 		all->cmd = ft_split_commas(tmp, ' ');
